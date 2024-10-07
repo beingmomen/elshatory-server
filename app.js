d0af8f5a410b97778eca6ffb340eb5c30998615d
Maron-X <120732521+Maron-X@users.noreply.github.com>
2024-10-07 14:16:28 +0300

    🎨

--- app.js
+++ app.js
@@ -80,6 +80,7 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.use('/api/v1/categories', categoryRouter);
+// app.use('/api/v1/categories', categoryRouter);
 app.use('/api/v1/testimonials', testimonialRouter);
 
 // Main Routes
